f527e49ea2e13f90076f2d0819102ae4b4d9b53b
Baptiste Candellier <outadoc@gmail.com>
2016-10-30 15:52:37 +0100

    Make Linky a module, add test script

--- linky.py
+++ linky.py
@@ -3,7 +3,6 @@
 
 import requests
 import base64
-import os
 from enum import Enum
 
 LOGIN_BASE_URI = 'https://espace-client-connexion.erdf.fr'
@@ -11,9 +10,6 @@ API_BASE_URI = 'https://espace-client-particuliers.erdf.fr/group/espace-particul
 
 API_ENDPOINT_LOGIN = '/auth/UI/Login'
 API_ENDPOINT_DATA = '/jeconsultetelechargemesdonneesdeconsommation'
-
-username = os.environ['LINKY_USERNAME']
-password = os.environ['LINKY_PASSWORD']
 
 class LinkyLoginException(Exception):
     pass
@@ -34,18 +30,18 @@ def login(username, password):
     return session_cookie
 
 def get_data_hour(token, start_date, end_date):
-    return get_data(token, 'urlCdcHeure', start_date, end_date)
+    return _get_data(token, 'urlCdcHeure', start_date, end_date)
 
 def get_data_day(token, start_date, end_date):
-    return get_data(token, 'urlCdcJour', start_date, end_date)
+    return _get_data(token, 'urlCdcJour', start_date, end_date)
 
 def get_data_month(token, start_date, end_date):
-    return get_data(token, 'urlCdcMois', start_date, end_date)
+    return _get_data(token, 'urlCdcMois', start_date, end_date)
 
 def get_data_year(token):
-    return get_data(token, 'urlCdcAn')
+    return _get_data(token, 'urlCdcAn')
 
-def get_data(token, resource_id, start_date = None, end_date = None):
+def _get_data(token, resource_id, start_date = None, end_date = None):
     cookies = {'iPlanetDirectoryPro': token}
     payload = {
         '_lincspartdisplaycdc_WAR_lincspartcdcportlet_INSTANCE_partlincspartcdcportlet_dateDebut': start_date,
@@ -65,12 +61,3 @@ def get_data(token, resource_id, start_date = None, end_date = None):
 
     r = requests.post(API_BASE_URI + API_ENDPOINT_DATA, allow_redirects=False, cookies=cookies, data=payload, params=params)
     return r.json()
-
-try:
-    print("logging in as " + username + "...")
-    token = login(username, password)
-    print("logged in successfully")
-    res = get_data_day(token, '27/10/2016', '30/10/2016')
-    print(res)
-except LinkyLoginException as e:
-    print(e)
---
+++ linky_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import linky
+import os
+
+username = os.environ['LINKY_USERNAME']
+password = os.environ['LINKY_PASSWORD']
+
+try:
+    print("logging in as " + username + "...")
+    token = linky.login(username, password)
+    print("logged in successfully")
+    res = linky.get_data_day(token, '27/10/2016', '30/10/2016')
+    print(res)
+except LinkyLoginException as e:
+    print(e)
