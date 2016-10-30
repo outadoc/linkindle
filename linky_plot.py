f9cebe8e6c24699e1030b23efcae98c2a36af6aa
Baptiste Candellier <outadoc@gmail.com>
2016-10-30 20:56:48 +0100

    Added hourly data

--- linky_plot.py
+++ linky_plot.py
@@ -27,31 +27,29 @@ def generate_y_axis(res):
 
     return y_values
 
-def generate_x_axis(res, time_delta_unit, time_format):
+def generate_x_axis(res, time_delta_unit, time_format, inc):
     x_values = []
 
     start_date_queried_str = res['graphe']['periode']['dateDebut']
     start_date_queried = datetime.strptime(start_date_queried_str, "%d/%m/%Y").date()
 
     kwargs = {}
-    kwargs[time_delta_unit] = res['graphe']['decalage']
+    kwargs[time_delta_unit] = res['graphe']['decalage'] * inc
     start_date = start_date_queried - relativedelta(**kwargs)
 
     for ordre, datapoint in enumerate(res['graphe']['data']):
         kwargs = {}
-        kwargs[time_delta_unit] = ordre
+        kwargs[time_delta_unit] = ordre * inc
         x_values.insert(ordre, (start_date + relativedelta(**kwargs)).strftime(time_format))
 
     return x_values
 
-def generate_graph_from_data(res, title, time_delta_unit, time_format, ylegend):
+def generate_graph_from_data(res, title, time_delta_unit, time_format, ylegend, inc = 1):
     y_values = generate_y_axis(res)
-    x_values = generate_x_axis(res, time_delta_unit, time_format)
-
-    print(y_values)
-    print(x_values)
+    x_values = generate_x_axis(res, time_delta_unit, time_format, inc)
 
     width = .55
+    max_power = res['graphe']['puissanceSouscrite']
 
     fig = plot.figure(num=None, figsize=(4.8, 3.6), dpi=166.6666, facecolor='w', edgecolor='k')
     ind = np.arange(len(x_values))
@@ -66,15 +64,32 @@ def generate_graph_from_data(res, title, time_delta_unit, time_format, ylegend):
     plot.xlim([-width, len(x_values)])
     plot.title(title)
 
+    if max_power > 0:
+        plot.ylim([0, max_power])
+
     # If there are too many elements on the X axis, make it more compact
     if len(x_values) > 20:
         # Rotate labels
         fig.autofmt_xdate()
-        for label in ax.xaxis.get_ticklabels()[::2]:
+
+        if len(x_values) > 40:
+            # If there are waaaaay too many elements, hide all labels
+            # and only keep on out of four visible (plus the last one)
+            for label in ax.xaxis.get_ticklabels():
+                label.set_visible(False)
+            for label in ax.xaxis.get_ticklabels()[::4]:
+                label.set_visible(True)
+            ax.xaxis.get_ticklabels()[-1].set_visible(True)
+        else:
             # Hide every other label
-            label.set_visible(False)
+            for label in ax.xaxis.get_ticklabels()[::2]:
+                label.set_visible(False)
 
     return plot
+
+def generate_graph_hours(res):
+    plot = generate_graph_from_data(res, "Puissance atteinte par demi-heure", 'hours', "%H:%M", "kW", 0.5)
+    plot.savefig(output_dir + "/linky_hours.png")
 
 def generate_graph_days(res):
     plot = generate_graph_from_data(res, "Consommation d'électricité par jour", 'days', "%d %b", "kWh")
@@ -97,12 +112,14 @@ try:
     res_month = linky.get_data_per_month(token, '01/05/2016', '30/10/2016')
     res_year = linky.get_data_per_year(token)
     res_day = linky.get_data_per_day(token, '29/09/2016', '29/10/2016')
+    res_hour = linky.get_data_per_hour(token, '29/10/2016', '30/10/2016')
     print("got data!")
 
     print("generating graphs...")
     generate_graph_months(res_month)
     generate_graph_years(res_year)
     generate_graph_days(res_day)
+    generate_graph_hours(res_hour)
     print("successfully generated graphs!")
 
 except linky.LinkyLoginException as e:
