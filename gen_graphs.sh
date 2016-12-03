dd55ea04011df0954336b8fa13e053365c04a2b7
Baptiste Candellier <outadoc@gmail.com>
2016-12-03 12:37:13 +0100

    Only run mogrify if successful

--- gen_graphs.sh
+++ gen_graphs.sh
@@ -3,6 +3,10 @@
 export LINKY_USERNAME=""
 export LINKY_PASSWORD=""
 
-python3 /home/iot/linkindle/linky_plot.py >> /var/log/linky.log 2>&1
-mogrify -rotate 90 -resize "600x800!" -define png:color-type=0 -define png:bit-depth=8 -dither FloydSteinberg -remap kindle_colors.gif /var/www/html/linky/*.png >> /var/log/linky.log 2>&1
+processimgs ()
+{
+    mogrify -rotate 90 -resize "600x800!" -define png:color-type=0 -define png:bit-depth=8 -dither FloydSteinberg -remap kindle_colors.gif /var/www/html/linky/*.png >> /var/log/linky.log 2>&1
+}
+
+python3 /home/iot/linkindle/linky_plot.py >> /var/log/linky.log 2>&1 && processimgs
 
