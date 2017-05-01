27d8fa1e45a22139c695e25d99dc4f56e4d3ee73
Baptiste Candellier <outadoc@gmail.com>
2017-05-01 23:15:46 +0200

    Fix issue where graphs couldn't be fetched anymore.
    The problem was fixed by calling the API twice in a row when we get redirected.
    Apparently, the 302 just wants us to... keep away for a while before we retry?
    Don't even need to follow the redirection. Just retry. That's nice.
    
    Also keep all cookies in a session because that helps.

--- linky.py
+++ linky.py
@@ -21,10 +21,11 @@
 import base64
 import requests
 
-LOGIN_BASE_URI = 'https://espace-client-connexion.erdf.fr'
-API_BASE_URI = 'https://espace-client-particuliers.erdf.fr/group/espace-particuliers'
+LOGIN_BASE_URI = 'https://espace-client-connexion.enedis.fr'
+API_BASE_URI = 'https://espace-client-particuliers.enedis.fr/group/espace-particuliers'
 
 API_ENDPOINT_LOGIN = '/auth/UI/Login'
+API_ENDPOINT_HOME = '/home'
 API_ENDPOINT_DATA = '/jeconsultetelechargemesdonneesdeconsommation'
 
 DATA_NOT_REQUESTED = -1
@@ -37,47 +38,48 @@ class LinkyLoginException(Exception):
 def login(username, password):
     """Logs the user into the Linky API.
     """
+    session = requests.Session()
+
     payload = {'IDToken1': username,
                'IDToken2': password,
                'SunQueryParamsString': base64.b64encode(b'realm=particuliers'),
                'encoded': 'true',
                'gx_charset': 'UTF-8'}
 
-    req = requests.post(LOGIN_BASE_URI + API_ENDPOINT_LOGIN, data=payload, allow_redirects=False)
-    session_cookie = req.cookies.get('iPlanetDirectoryPro')
+    req = session.post(LOGIN_BASE_URI + API_ENDPOINT_LOGIN, data=payload, allow_redirects=False)
 
-    if session_cookie is None:
+    if not 'iPlanetDirectoryPro' in session.cookies:
         raise LinkyLoginException("Login unsuccessful. Check your credentials.")
 
-    return session_cookie
+    return session
 
-def get_data_per_hour(token, start_date, end_date):
+def get_data_per_hour(session, start_date, end_date):
     """Retreives hourly energy consumption data."""
-    return _get_data(token, 'urlCdcHeure', start_date, end_date)
+    return _get_data(session, 'urlCdcHeure', start_date, end_date)
 
-def get_data_per_day(token, start_date, end_date):
+def get_data_per_day(session, start_date, end_date):
     """Retreives daily energy consumption data."""
-    return _get_data(token, 'urlCdcJour', start_date, end_date)
+    return _get_data(session, 'urlCdcJour', start_date, end_date)
 
-def get_data_per_month(token, start_date, end_date):
+def get_data_per_month(session, start_date, end_date):
     """Retreives monthly energy consumption data."""
-    return _get_data(token, 'urlCdcMois', start_date, end_date)
+    return _get_data(session, 'urlCdcMois', start_date, end_date)
 
-def get_data_per_year(token):
+def get_data_per_year(session):
     """Retreives yearly energy consumption data."""
-    return _get_data(token, 'urlCdcAn')
+    return _get_data(session, 'urlCdcAn')
 
-def _get_data(token, resource_id, start_date=None, end_date=None):
-    prefix = '_lincspartdisplaycdc_WAR_lincspartcdcportlet_INSTANCE_partlincspartcdcportlet_'
+def _get_data(session, resource_id, start_date=None, end_date=None):
+    iden = 'lincspartdisplaycdc_WAR_lincspartcdcportlet_INSTANCE_partlincspartcdcportlet'
 
     # We send the session token so that the server knows who we are
-    cookies = {'iPlanetDirectoryPro': token}
     payload = {
-        prefix + 'dateDebut': start_date,
-        prefix + 'dateFin': end_date
+        '_' + iden + '_dateDebut': start_date,
+        '_' + iden + '_dateFin': end_date
     }
+
     params = {
-        'p_p_id': 'lincspartdisplaycdc_WAR_lincspartcdcportlet_INSTANCE_partlincspartcdcportlet',
+        'p_p_id': iden,
         'p_p_lifecycle': 2,
         'p_p_state': 'normal',
         'p_p_mode': 'view',
@@ -88,6 +90,13 @@ def _get_data(token, resource_id, start_date=None, end_date=None):
         'p_p_col_count': 3
     }
 
-    req = requests.post(API_BASE_URI + API_ENDPOINT_DATA, \
-                        allow_redirects=False, cookies=cookies, data=payload, params=params)
+    req = session.post(API_BASE_URI + API_ENDPOINT_DATA, \
+                        allow_redirects=False, data=payload, params=params)
+
+    if req.status_code >= 300 and req.status_code < 400:
+        # So... apparently, we may need to do that once again if we hit a 302
+        # ¯\_(ツ)_/¯
+        req = session.post(API_BASE_URI + API_ENDPOINT_DATA, \
+                            allow_redirects=False, data=payload, params=params)
+
     return req.json()
